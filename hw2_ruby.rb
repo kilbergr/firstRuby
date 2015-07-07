155c7132e7651163590dbedceab4702f2ef939ed
kilbergr <rekilberg@gmail.com>
2015-07-06 21:13:15 -0700

    with multiplication table

--- hw2_ruby.rb
+++ hw2_ruby.rb
@@ -44,3 +44,67 @@ char = gets.chomp.to_s
 for current_row in 1..rows
 	puts " "*(rows-current_row) + char*((current_row*2)-1)
 end
+
+# #Mult table
+puts "Specify size of multiplication chart (i.e. specifying '9' will result in a chart with 81 entries)"
+input = gets.chomp.to_i
+for current_col in 1..input
+	print current_col.to_s + "|  "
+	current_row = 1
+	while current_row<= input
+		if current_row*current_col < 10
+			print (current_row*current_col).to_s + "   "
+		else
+		print (current_row*current_col).to_s + "  "
+	end
+		current_row+=1
+	end
+	print "\n"
+end
+
+# reverse string
+# puts "Enter a string"
+# input = gets.chomp
+# for char in 0..input.length/2
+# 	for rev_char in 0..input.length
+# 		temp = input[char]
+# 		input[char] = input[rev_char-1]
+# 		input[rev_char-1] = temp[char]
+# 	end
+# 	puts input
+# end
+
+
+
+# # reverse string
+# puts "Enter a string"
+# input = gets.chomp
+# arr = input.split("")
+# for char in 0..arr.length/2
+# 	for rev_char in 0..arr.length/2
+# 		temp = arr[char]
+# 		arr[char] = arr[arr.length-1-rev_char]
+# 		arr[arr.length-1-rev_char] = temp
+# 	end
+# 	reverse_str = arr.join("")
+# end
+# puts reverse_str
+
+# # reverse string
+# puts "Enter a string"
+# input = gets.chomp
+# arr = input.split("")
+# rev_char = arr.length-1
+# arr.each do |char|
+# 	temp = arr[char]
+# 	arr[char] = 
+# for char in 0..arr.length/2
+# 	while rev_char > 0
+# 		temp = arr[char]
+# 		arr[char] = arr[rev_char]
+# 		arr[rev_char] = temp
+# 		rev_char-=1
+#  	end
+# 	reverse_str = arr.join("")
+# end
+# puts reverse_str
