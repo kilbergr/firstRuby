8fb314cc374077012664a528bbfa4c97f1763c0f
kilbergr <rekilberg@gmail.com>
2015-07-07 09:26:08 -0700

    more elegant solutions

--- hw2_ruby.rb
+++ hw2_ruby.rb
@@ -45,6 +45,15 @@ for current_row in 1..rows
 	puts " "*(rows-current_row) + char*((current_row*2)-1)
 end
 
+# Alt ASCII art
+char = "*"
+rows = 5
+count = 1
+rows.times do
+	puts (char * count).center(9)
+	count += 2
+end
+
 #Mult table
 puts "Specify size of multiplication chart (i.e. specifying '9' will result in a chart with 81 entries)"
 input = gets.chomp.to_i
@@ -74,40 +83,22 @@ arr = input.split("")
 current_char = 0
 last_char = arr.length-1
 
-	while current_char < arr.length/2
-		temp = arr[last_char-current_char]
-		arr[last_char - current_char] = arr[current_char]
-		arr[current_char] = temp
-		current_char+=1
- 	end
-	reverse_str = arr.join("")
-	puts reverse_str
+while current_char < arr.length/2
+	temp = arr[last_char-current_char]
+	arr[last_char - current_char] = arr[current_char]
+	arr[current_char] = temp
+	current_char+=1
+end
+reverse_str = arr.join("")
+puts reverse_str
 
-	
-# reverse string
-# puts "Enter a string"
-# input = gets.chomp
-# for char in 0..input.length/2
-# 	for rev_char in 0..input.length
-# 		temp = input[char]
-# 		input[char] = input[rev_char-1]
-# 		input[rev_char-1] = temp[char]
-# 	end
-# 	puts input
-# end
-
-
-
-# # reverse string
-# puts "Enter a string"
-# input = gets.chomp
-# arr = input.split("")
-# for char in 0..arr.length/2
-# 	for rev_char in 0..arr.length/2
-# 		temp = arr[char]
-# 		arr[char] = arr[arr.length-1-rev_char]
-# 		arr[arr.length-1-rev_char] = temp
-# 	end
-# 	reverse_str = arr.join("")
-# end
-# puts reverse_str
+# REVERSING IN PLACE
+def reverse_in_place
+	puts "Enter your string"
+	str = gets.chomp.to_s
+	mid = str.length/2
+	(0..mid).each do |index|
+		str[index], str[-index-1] = str[-index-1], str[index]
+		end
+	puts str
+end
